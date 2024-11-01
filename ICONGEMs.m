24c94aa06ef6401b6693c4dd5f0eec97298a977c
ThummaratPaklao <81726643+ThummaratPaklao@users.noreply.github.com>
2024-11-01 16:03:25 +0700

    Update ICONGEMs.m

--- ICONGEMs.m
+++ ICONGEMs.m
@@ -1,248 +1,305 @@
-function solution = ICONGEMs(model,exp,txt1,condition,threashold,alpha)
-% Use this algorithm to integrate a co-expression network and genome scale 
-% metabolic model. This algorithm calculate the reaction flux distribution
-% in each condition by applying quadratic programming.
+function [solICONGEMs, boundEf] = ICONGEMs(model, exp, genetxt, condition, threshold, alpha, numericFlag)
+% Algorithm to Integrate a Gene Co-expression Network and Genome-scale Metabolic Model:
+% This algorithm calculates the reaction flux distribution for each condition by applying 
+% quadratic programming.
 %
 % USAGE:
 %
-%    solution = ICONGEMs(model,exp,txt1,condition,threashold,alpha)
+%    [solICONGEMs, boundEf] = ICONGEMs(model, exp, genetxt, condition, threashold, alpha)
 %
 % INPUTS:
 %
-%    model:               input model (COBRA model structure)
-%    exp:                 expression profile corresponding gene names that
-%                         extract from gene expression profile file
-%    txt1:                text that extract from gene expression profile
-%                         file
+%    model:           input model (COBRA model structure)
+%    exp:             expression profile corresponding to the gene names that 
+%                     extract from gene expression profile file
+%    genetxt:         list of gene names that extract from gene expression profile
+%                     file
+%    numericFlag:     1 if using Human Recon  (Default = 0).
 %
-% OPTIONAL INPUT:
-%    threshold:           value of correlation coefficient for construct 
-%                         co-expression network (default value 0.9)
-%    condition:           row vector of number of condition corresponding 
-%                         condition in exp
-%                         (default value - 1:size(exp,2))
-%    alpha:               The value for proportion of biomass (default value 1) 
+% OPTIONAL INPUTS:
+%    threshold:           The value of the correlation coefficient for constructing 
+%                         the co-expression network (default value: 0.9).
+%    condition:           Row vector indicating the number of conditions 
+%                         corresponding to the conditions in exp 
+%                         (default value: 1:size(exp, 2)).
+%    alpha:               The value for the proportion of biomass (default value: 1).
 %                         
 % OUTPUTS:
-%    solution:             flux distribution table corresponding reaction
-%                          flux names
+%    solICONGEMs:             Flux distribution table corresponding to reaction flux names.
+%    boundEf:                 Upper bound of E-flux.
 %
-% 
-if nargin< 4 || isempty(condition)
-      condition =1:size(exp,2);
+% EXAMPLES:
+%    % This could be an example that can be copied from the documentation to MATLAB:
+%    solution = ICONGEMs(model, exp, genetxt, condition, threashold, alpha)
+%    % without optional values:
+%    solution = ICONGEMs(model, exp, genetxt)
+%
+%..Author: 
+%    -Thummarat Paklao, 01/02/2024, Department of Mathematics and Computer Science, Faculty of Science, Chulalongkorn University, Thailand. 
+%    -Apichat Suratanee, 01/02/2024, Department of Mathematics, Faculty of Applied Science, King Mongkut's University of Technology North Bangkok. 
+%    -Kitiporn Plaimas, 01/02/2024, Department of Mathematics and Computer Science, Faculty of Science, Chulalongkorn University, Thailand.    
+
+if (nargin < 4 || isempty(condition))
+      condition = 1:size(exp, 2);
 end
-if nargin< 5 ||isempty(threashold)
-      threashold=0.9;
+if (nargin < 5 || isempty(threshold))
+      threshold = 0.9;
 end
-if nargin< 6 ||isempty(alpha)
-      alpha=0.99;
+if (nargin < 6 || isempty(alpha))
+      alpha = 0.99;
+end
+if (nargin < 7 || isempty(numericFlag))
+      numericFlag = 0;
 end
 
 % construct the template model
-model_n=model;
-model_n.lb(model_n.lb>=0)=0;
-model_n.lb(model_n.lb<0)=-1000;
-model_n.ub(model_n.ub<=0)=0;
-model_n.ub(model_n.ub>0)=1000;
 
-%convert to irreversible format
-[modelIrrev,matchRev,rev2irrev,irrev2rev] = convertToIrreversible(model_n);
+modelN = model;
+modelN.lb(modelN.lb >= 0) = 0;
+modelN.lb(modelN.lb < 0) = -1000;
+modelN.ub(modelN.ub <= 0) = 0;
+modelN.ub(modelN.ub > 0) = 1000;
 
-%check gene name in gene expression profile agree gene name in metabolic
-%model
-geneinMet=zeros(size(txt1,1)-1,1);
-for i=2:size(txt1,1)
-    for j=1:size(modelIrrev.genes,1)
-        if string(txt1(i))==string(modelIrrev.genes(j))
-            geneinMet(i-1)=1;
+% convert to irreversible format
+
+[modelIrrev, matchRev, rev2irrev, irrev2rev] = convertToIrreversible(modelN);
+
+% Check that the gene names in the gene expression profile agree with 
+% the gene names in the metabolic model.
+
+geneinMet = zeros(size(genetxt, 1) - 1, 1);
+for i = 2:size(genetxt, 1)
+    for j = 1:size(modelIrrev.genes, 1)
+        if string(genetxt(i)) == string(modelIrrev.genes(j))
+            geneinMet(i - 1) = 1;
         end
     end
 end
-if sum(sum(geneinMet))==0
+if sum(sum(geneinMet)) == 0
     disp("gene name in gene expression data is not agree gene name in metabolic model")
 end
 
-%Choose only gene expression profile agree gene in metabolic model
-exp1=exp(geneinMet==1,:);
-txt2=txt1([0;geneinMet]==1,1);
+% Choose only the gene expression profiles that match genes in the metabolic model.
 
-%remove missing data
-[exp1,TF]=rmmissing(exp1,1);
-txt2=txt2(~TF,1);
+exp1 = exp(geneinMet == 1, :);
+txt2 = genetxt([0; geneinMet] == 1, 1);
 
-%Construct co-expression network 
-cor=corr((exp1)');
-cor1=cor>=threashold;
+% remove missing data
 
-%find gene pair that have hihg correlation in co-expression network
-ind_cor_gene=zeros(1,2);
-l=1;
-for i =1:size(cor,1)
-    for j =i:size(cor,1)
-        if (cor1(i,j)~=0)&&(i~=j)
-            ind_cor_gene(l,1)=i;
-            ind_cor_gene(l,2)=j;
-            l=l+1;
+[exp1, TF] = rmmissing(exp1, 1);
+txt2 = txt2(~TF, 1);
+geneincoexnet = modelIrrev.genes(~TF);
+
+% Construct co-expression network 
+
+cor = corr((exp1)');
+cor1 = cor >= threshold;
+
+% Find gene pairs that have a high correlation in the co-expression network.
+
+indCorGene = zeros(1, 2);
+l = 1;
+for i = 1:size(cor, 1)
+    for j = i:size(cor,1)
+        if (cor1(i,j) ~= 0) && (i ~= j)
+            indCorGene(l, 1) = i;
+            indCorGene(l, 2) = j;
+            l = l + 1;
          end
     end
 end
 
-co_gene=cell(size(ind_cor_gene,1),2);
-co_gene(:,1)=geneincoexnet(ind_cor_gene(:,1),1);
-co_gene(:,2)=geneincoexnet(ind_cor_gene(:,2),1);
+coGene = cell(size(indCorGene, 1), 2);
+coGene(:, 1) = geneincoexnet(indCorGene(:, 1), 1);
+coGene(:, 2) = geneincoexnet(indCorGene(:, 2), 1);
 
-Name_rxn={};
-for i=1:size(modelIrrev.genes)
-    [z1,Name_rxn{i}]=findRxnsFromGenes(modelIrrev, modelIrrev.genes(i,1),0,1);
+NameRxn={};
+for i = 1:size(modelIrrev.genes)
+    [z1, NameRxn{i}] = findRxnsFromGenes(modelIrrev, modelIrrev.genes(i, 1), numericFlag, 1);
 end
 
-%find reaction that satisfies gene 
-Name_cor_rxn={};
-for i=1:size(co_gene,1)
-    Name_cor_rxn{i,1}=Name_rxn{find(modelIrrev.genes==string(co_gene(i,1)))};
-    Name_cor_rxn{i,2}=Name_rxn{find(modelIrrev.genes==string(co_gene(i,2)))};
+% Find reactions that correspond to the gene
+
+NameCorRxn = {};
+for i = 1:size(coGene, 1)
+    NameCorRxn{i, 1} = NameRxn{find(modelIrrev.genes == string(coGene(i, 1)))};
+    NameCorRxn{i, 2} = NameRxn{find(modelIrrev.genes == string(coGene(i, 2)))};
 end
 
-f=0;
-h=0;
-ind_cor_rxn=zeros(10,2);
-for i=1:size(Name_cor_rxn,1)
-    if ~isempty(Name_cor_rxn{i,1}) && ~isempty(Name_cor_rxn{i,2})
-    for j=1:size(Name_cor_rxn{i,1},1)
-        f=f+size(Name_cor_rxn{i,2},1);
-        ind_cor_rxn(h+1:h+size(Name_cor_rxn{i,2},1),1)=findRxnIDs(modelIrrev,Name_cor_rxn{i,1}{j,1}); 
-            for w=1:size(Name_cor_rxn{i,2},1)
-                ind_cor_rxn(h+w,2)=findRxnIDs(modelIrrev,Name_cor_rxn{i,2}{w,1}); 
+f = 0;
+h = 0;
+indCorRxn = zeros(10, 2);
+for i = 1:size(NameCorRxn, 1)
+    if (~isempty(NameCorRxn{i, 1}) && ~isempty(NameCorRxn{i, 2}))
+    for j = 1:size(NameCorRxn{i, 1}, 1)
+        f = f + size(NameCorRxn{i, 2}, 1);
+        indCorRxn(h + 1 : h + size(NameCorRxn{i, 2}, 1), 1) = findRxnIDs(modelIrrev, NameCorRxn{i, 1}{j, 1}); 
+            for w = 1:size(NameCorRxn{i, 2}, 1)
+                indCorRxn(h + w,2)=findRxnIDs(modelIrrev,NameCorRxn{i, 2}{w, 1}); 
             end
-            h=f;
+            h = f;
     end
     end
 end
 
-%construct reaction pair matrix
-R=zeros(length(modelIrrev.rxns),length(modelIrrev.rxns));
-for i=1:size(ind_cor_rxn,1)
-    if ind_cor_rxn(i,1)~=ind_cor_rxn(i,2)
-        R(ind_cor_rxn(i,1),ind_cor_rxn(i,2))= 1;       
+% Construct a reaction pair matrix.
+
+R = zeros(length(modelIrrev.rxns),length(modelIrrev.rxns));
+for i = 1:size(indCorRxn, 1)
+    if indCorRxn(i, 1) ~= indCorRxn(i, 2)
+        R(indCorRxn(i, 1), indCorRxn(i, 2)) = 1;       
     end
 end
 
-%construct irreversible reaction matrix that are decomposed from  the same reversible reaction 
-Re=zeros(size(R));
-for i=1:length(model.rxns)
-    if length(rev2irrev{i,1})==2
-        Re(i,rev2irrev{i,1}(2))=1;
+% Construct an irreversible reaction matrix from the reversible reactions 
+% that are decomposed into the same components.
+
+Re = zeros(size(R));
+for i = 1:length(model.rxns)
+    if length(rev2irrev{i, 1}) == 2
+        Re(i, rev2irrev{i,1}(2)) = 1;
+    end
+end
+
+% Process gene expression data based on Gene-Protien-Reaction (GPR) associations.
+
+geneExdat = zeros(length(modelIrrev.genes), 1);
+nupb = zeros(length(modelIrrev.rules), size(exp, 2));
+for ch = 1:size(exp, 2)
+for i = 1:length(modelIrrev.genes)
+    cc = 0;
+    for j = 1:length(genetxt(:, 1)) - 1
+        if string(modelIrrev.genes(i)) == string(genetxt(j + 1, 1))
+            geneExdat(i) = exp(j, ch);
+            cc = cc + 1;
         end
-end
-
-%process gene expression data based on GPR association
-gene_exdat=zeros(length(modelIrrev.genes),1);
-nupb=zeros(length(modelIrrev.rules),size(exp,2));
-for ch=1:size(exp,2)
-for i=1:length(modelIrrev.genes)
-    cc=0;
-    for j=1:length(txt1(:,1))-1
-        if string(modelIrrev.genes(i))==string(txt1(j+1,1))
-            gene_exdat(i)=exp(j,ch);
-            cc=cc+1;
-        end
     end
-    if gene_exdat(i)==0 && cc==0
-        gene_exdat(i)=1000;
+    if geneExdat(i) == 0 && cc == 0
+        geneExdat(i) = 1000;
     end
 end
 
 for i = 1:length(modelIrrev.rules)
-    rulegene=modelIrrev.rules{i};
+    rulegene = modelIrrev.rules{i};
     if isempty(rulegene)
-        rsum=1000;
+        rsum = 1000;
     else
         newrule = split(rulegene,"|");
-        nrule=size(newrule,1);
-        rsum=0;
-        for j=1:nrule
-            newrule1=newrule{j};
-            nnrule=length(newrule1);
-            rmin=inf;
-            for k=1:nnrule
-                if newrule1(k)=='x'
-                    r1=k+2;
+        nrule = size(newrule, 1);
+        rsum = 0;
+        for j = 1:nrule
+            newrule1 = newrule{j};
+            nnrule = length(newrule1);
+            rmin = inf;
+            for k = 1:nnrule
+                if newrule1(k) == 'x'
+                    r1 = k+2;
                 end
-                if newrule1(k)==')' && newrule1(k-1)~=' ' && newrule1(k-1)~=')'
-                    rmin=min(rmin,gene_exdat(str2num(convertCharsToStrings((newrule1(r1:k-1))))));
+                if (newrule1(k) == ')' && newrule1(k-1) ~= ' ' && newrule1(k - 1) ~= ')')
+                    rmin = min(rmin, geneExdat(str2num(convertCharsToStrings((newrule1(r1:k - 1))))));
                 end    
             end
-            rsum=rsum+rmin;
+            rsum = rsum + rmin;
         end 
     end
-    nupb(i,ch)=rsum;
+    nupb(i, ch) = rsum;
 end
 
 end
-pos_nupb=nupb>=1000;
-nupb(pos_nupb)=max(nupb(~pos_nupb));
+PosNupb = nupb >= 1000;
+nupb(PosNupb) = max(nupb(~PosNupb));
 
-%construct table for reporting the result
-solution=table(model.rxns);
- n=0;
+% Construct a table for reporting the results
+
+solution = table(model.rxns);
+solutionFull = table(modelIrrev.rxns);
+solutionEf = table(model.rxns);
+solutionEfFull = table(modelIrrev.rxns);
+n = 0;
  
-%construct model and calculate flux distribution
-for ch=condition
-    n=n+1;
+% Construct the model and calculate the flux distribution.
+
+for ch = condition
+    n = n + 1;
 disp("Condition :"); disp(ch);
-model3=changeRxnBounds(modelIrrev,modelIrrev.rxns, nupb(:,ch),'u');
+model3 = changeRxnBounds(modelIrrev,modelIrrev.rxns, nupb(:, ch), 'u');
 solution1 = optimizeCbModel(model3);
 
-Trans0=zeros(size(modelIrrev.mets,1),size(modelIrrev.rxns,1));
-Trans2=-1*eye(size(modelIrrev.rxns,1));
-S2=zeros(size(modelIrrev.rxns,1));
-for i=1:length(modelIrrev.rxns)
-    S2(i,i)=1/max(nupb(:,ch));
+Trans0 = zeros(size(modelIrrev.mets, 1),size(modelIrrev.rxns, 1));
+Trans2 = -1 * eye(size(modelIrrev.rxns, 1));
+S2 = zeros(size(modelIrrev.rxns, 1));
+for i = 1:length(modelIrrev.rxns)
+    S2(i, i) = 1 / max(nupb(i, :));
 end
 
-Obj4=[modelIrrev.c' zeros(1,size(modelIrrev.rxns,1))] ;
 
-lob=[model3.lb; (-1)*inf*ones(size(modelIrrev.rxns,1),1)];
-upb=[model3.ub; inf*ones(size(modelIrrev.rxns,1),1)];
+Obj4 = [modelIrrev.c' zeros(1, size(modelIrrev.rxns, 1))] ;
 
-O=[zeros(size(R)) zeros(size(R)) ; zeros(size(R)) R]; 
-Aeq=[modelIrrev.S Trans0 ; S2 Trans2 ; Obj4]; 
-beq=[zeros(size(modelIrrev.mets,1),1); (-1)*ones(size(modelIrrev.rxns,1),1) ; alpha*solution1.f];
+lob = [model3.lb; (-1) * inf * ones(size(modelIrrev.rxns, 1), 1)];
+upb = [model3.ub; inf * ones(size(modelIrrev.rxns, 1), 1)];
 
-model2=struct;
-model2.lb=lob;
-model2.ub=upb; 
-model2.A=sparse(Aeq);
-model2.sense = [char('='* ones(size(model2.A,1)-1, 1)) ; char('>')]; 
-model2.rhs=beq;
+O = [zeros(size(R)) zeros(size(R)); zeros(size(R)) R]; 
+Aeq = [modelIrrev.S Trans0; S2 Trans2; Obj4]; 
+beq = [zeros(size(modelIrrev.mets, 1), 1); (-1) * ones(size(modelIrrev.rxns,1),1); alpha * solution1.f];
+
+model2 = struct;
+model2.lb = lob;
+model2.ub = upb; 
+model2.A = sparse(Aeq);
+model2.sense = [char('=' * ones(size(model2.A,1) - 1, 1)) ; char('>')]; 
+model2.rhs = beq;
 model2.modelsense = 'max'; 
-numrxn=[1:length(modelIrrev.rxns)]; 
-j=1;
-for i=1:length(model.rxns)
-    if length(Re(Re(i,:)>0))==1
+numrxn = [1:length(modelIrrev.rxns)]; 
+j = 1;
+for i = 1:length(model.rxns)
+    if length(Re(Re(i, :)>0)) == 1
 model2.quadcon(j).Qrow = i;
-model2.quadcon(j).Qcol = numrxn(Re(i,:)>0);
+model2.quadcon(j).Qcol = numrxn(Re(i, :)>0);
 model2.quadcon(j).Qval = 1.0;
 model2.quadcon(j).rhs = 0.0;
-model2.quadcon(j).q = sparse(zeros(2*size(Re,1),1));
+model2.quadcon(j).q = sparse(zeros(2*size(Re, 1), 1));
 model2.quadcon(j).sense = '=';
-j=j+1;
+j = j + 1;
     end
 end
 model2.Q = sparse(O);
 params.NonConvex = 2;
 result = gurobi(model2, params);
-x=result.x;
-sol_flux=[];
-for i=1:length(model.rxns)
-    if length(rev2irrev{i,1})==1
-        sol_flux(i,1)=x(i,1);
+x = result.x;
+solutionFull(:, n + 1) = table(x(1:size(modelIrrev.rxns),1));
+solutionEfFull(:, n + 1) = table(solution1.x);
+solFlux = [];
+solFluxEf = [];
+for i = 1:length(model.rxns)
+    if length(rev2irrev{i, 1}) == 1
+        solFlux(i, 1) = x(i, 1);
+        solFluxEf(i, 1) = solution1.x(i, 1);
     else
-        sol_flux(i,1)=(x(rev2irrev{i,1}(1,1))-x(rev2irrev{i,1}(1,2)));
+        solFlux(i, 1) = (x(rev2irrev{i, 1}(1, 1))-x(rev2irrev{i, 1}(1, 2)));
+        solFluxEf(i, 1) = (solution1.x(rev2irrev{i, 1}(1, 1))-solution1.x(rev2irrev{i, 1}(1, 2)));
+        if solFluxEf(i, 1)>=0
+            solutionEfFull{rev2irrev{i, 1}(1, 1), n + 1} = solFluxEf(i, 1);
+            solutionEfFull{rev2irrev{i, 1}(1, 2), n + 1} = 0;
+        elseif solFluxEf(i, 1)<0
+            solutionEfFull{rev2irrev{i, 1}(1, 1), n + 1} = 0;
+            solutionEfFull{rev2irrev{i, 1}(1, 2), n + 1} = abs(solFluxEf(i, 1));
+
+        end
     end
 end
-solution(:,n+1)=table(sol_flux);
+solution(:, n + 1) = table(solFlux);
+solutionEf(:, n + 1) = table(solFluxEf);
+
 end
+boundEf = table(nupb);
+solICONGEMs.sol = solution;
+solICONGEMs.solRev = solutionFull;
+
+solEflux.sol = solutionEf;
+solEflux.solRev = solutionEfFull;
+
+% Export the results
+
 filename = 'result.csv';
-writetable(solution,filename)
+writetable(solution, filename)
 end
